1db77d4e81bdb1fe61b5e682852886ccfceff5ba
reachgregpff <gregpffwdi2@gmail.com>
2015-10-14 16:08:05 +1100

    second version

--- memory.js
+++ memory.js
@@ -3,17 +3,33 @@ var game = {
   previousCardIndex: null,
   p1Score: 0,
   p2Score: 0,
+  flipTimer: null,
+  currentAvatarImage: "img/av00.png",
   allCards : ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png"],
   randomCardArray : [],
   getRandomCardArray: function(){
     game.randomCardArray = (_.shuffle(game.allCards)).slice(0, 6);  //0 including and 6 excluding
     game.randomCardArray = _.shuffle(game.randomCardArray.concat(game.randomCardArray));  //create duplicates, then shuffle again
     console.log(game.randomCardArray);
+  },
+  flipCard : function(flipIndex, image){
+    $card = $('.randomCards .randomCardImg').eq(flipIndex);
+    if(image === "flipagain"){
+      $card.attr("src", game.currentAvatarImage);
+      console.log("called by setInterval")
+    }else{
+      $card.attr("src", ("img/" + game.randomCardArray[flipIndex]));
+    }
+  },
+  flipCardTwice : function(flipIndex){
+    game.flipCard(flipIndex);
+    flipTimer = window.setInterval(function(){game.flipCard(flipIndex, "flipagain"); window.clearInterval(flipTimer);}, 1000);
   }
 
 }
 
-//HTML elements are set here
+//global variables here
+var cardsDiscovered = [false, false, false, false, false, false, false, false, false, false, false, false];
 
 function checkIfClickedCorrectly(){
 
@@ -23,51 +39,77 @@ function checkIfClickedCorrectly(){
     console.log("Previous Index is " + game.previousCardIndex + " & previous card clicked is " + game.randomCardArray[game.previousCardIndex]);
   }
 
-  if(game.previousCard === -1){  // Check if this is the first click
-    game.previousCard = currentCardIndex;    // This is the first click so return back
+  if(game.previousCardIndex === -1){  // Check if this is the first click
+    game.previousCardIndex = currentCardIndex;    // This is the first click so return back
+    game.flipCardTwice(currentCardIndex);  // Flip and then revert back to avatar
+    
     return;
   }
 
   if(currentCardIndex === game.previousCardIndex) {  //Check if the second card is the same as the first card
+    game.flipCardTwice(currentCardIndex);           // Flip and then revert back to avatar
     return;    //Same card clicked again so return back
   }
 
-  //this is the main logic of the game where you compare the current and previous cards
+  //#####################this is the main logic of the game where you compare the current and previous cards
   if(game.randomCardArray[currentCardIndex] === game.randomCardArray[game.previousCardIndex]){  //if card1 equals card2
     console.log("YAY!! you clicked " + game.randomCardArray[game.previousCardIndex] + " and " + game.randomCardArray[currentCardIndex]);
+    game.flipCard(currentCardIndex);
+    game.flipCard(game.previousCardIndex);  //flip both
+    
+    cardsDiscovered[currentCardIndex] = true;   //set these as discovered
+    cardsDiscovered[game.previousCardIndex] = true;
+
     game.previousCardIndex = -1;   // reset the previous card
   }else{
     game.previousCardIndex = currentCardIndex;
+    game.flipCardTwice(currentCardIndex);  // Flip and then revert back to avatar
   }
+  //##########################################################end of main logic
   
+}
+
+function refreshCardDisplay(){
+  //change the face of every random card
+  //$('.randomCards .randomCardImg').each(function(){  // USING JQUERY each just this once, use underscore next time
+  for(var i=0; i<cardsDiscovered.length; i++) {
+  //_.each($('.randomCards .randomCardImg'), index, function(){
+    if(cardsDiscovered[i]===false){
+      $('.randomCards .randomCardImg').eq(i).attr("src", game.currentAvatarImage);
+    }
+  }
 }
 
 function changeAvatar(){
 
-  var currentAvatarImage = $(this).attr('src');
+  game.currentAvatarImage = $(this).attr('src');
   //Change Player's profile image
   if(player1) {
-    $('#playerImage1').attr("src", currentAvatarImage);
+    $('#playerImage1').attr("src", game.currentAvatarImage);
   }else{
-    $('#playerImage2').attr("src", currentAvatarImage);
+    $('#playerImage2').attr("src", game.currentAvatarImage);
   }
-
-  //change the face of every random card
-  $('.randomCards .randomCardImg').each(function(){  // USING JQUERY each just this once, use underscore next time
-      $(this).attr("src", currentAvatarImage);
-  });
+  refreshCardDisplay();
 }
 
+function reset(){
+  //_.each(cardsDiscovered, function(elem){elem=false;});   //this underscore method is not working, why?
+  for(var i=0; i<cardsDiscovered.length; i++){
+    cardsDiscovered[i] = false;
+  }
+  game.previousCardIndex = -1;
+  refreshCardDisplay();
+}
 
 function startGameForPlayer1(){
   player1 = true;
-  game.previousCardIndex = -1;
+  reset();
   game.getRandomCardArray();
 }
 
 function startGameForPlayer2(){
   player1 = false;
-  game.previousCardIndex = -1;
+  reset();
   game.getRandomCardArray();
 }
 
